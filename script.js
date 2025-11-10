6f7fa9dfcd9a12ef458ae7d249a62e846e22d07d
Enzo Suzula <suzulaenzo@gmail.com>
2025-11-10 14:44:25 -0300

    Atualização IPTU AI

--- script.js
+++ script.js
@@ -23,10 +23,10 @@ document.addEventListener('DOMContentLoaded', function() {
         projects: "Projetos em Destaque",
         project1Title: "Portal do Contribuinte",
         project1Desc: "Sistema web desenvolvido para a Prefeitura de Duque de Caxias. Feito para ser algo didático e que atendesse clientes de todas as idades, permitindo acesso a informações cadastrais e financeiras dos imóveis de Duque de Caxias. Implementado com HTML, CSS, JavaScript e integração com banco de dados municipal.",
-        project2Title: "Site Loja de Roupas",
-        project2Desc: "Website moderno e responsivo desenvolvido para uma loja de roupas, com foco em design atrativo, praticidade e integração com carrinho de compras.",
+        project2Title: "IPTU IA ( Em aprimoramento )",
+        project2Desc: "Sistema web desenvolvido para a Prefeitura de Duque de Caxias. Sistema que utiliza IA para acelerar o processo de trabalho dos funcionários. Basicamente a IA pega uma imagem, depois é coletado os dados de dentro da imagem e automaticamente é preenchido a maioria dos dados, após clicar em gerar proposta, a IA gera propostas com valores que a Prefeitura utiliza para lidar da melhor forma com os contribuintes.",
       },
-      
+
       en: {
         title: "Full Stack Developer",
         bio: "I am enthusiastic about technology and developing innovative solutions. I am currently studying Software Engineering and always strive to create modern, high-performance projects with clean code. I have experience in Full Stack Web Development. I am currently learning React, Java, and JavaScript. I am always willing to learn more.",
@@ -34,8 +34,8 @@ document.addEventListener('DOMContentLoaded', function() {
         projects: "Featured Projects",
         project1Title: "Portal do Contribuinte",
         project1Desc: "Web system developed for the City Hall of Duque de Caxias. Designed to be educational and accessible to clients of all ages, it allows access to property registration and financial information. Built with HTML, CSS, JavaScript, and integrated with the municipal database.",
-        project2Title: "Clothing Store Website",
-        project2Desc: "A modern and responsive website developed for a clothing store, focusing on attractive design, usability, and shopping cart integration.",
+        project2Title: "IPTU AI ( Being improved )",
+        project2Desc: "Web system developed for the City Hall of Duque de Caxias. The system uses AI to accelerate employees’ workflow. Essentially, the AI processes an image, extracts data from it, and automatically fills in most of the required information. After clicking “Generate Proposal,” the AI creates proposals with values used by the City Hall to handle taxpayer matters more efficiently.",
       }
     };
 
